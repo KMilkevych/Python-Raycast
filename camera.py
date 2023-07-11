9a2ecc83e054cccb0113dc94997c717fd6335a79
Kostiantyn Milkevych <mkv2510kostya@gmail.com>
2023-07-11 17:10:27 +0200

    Buggy code, but now using numpy to avoid vertical loop

--- camera.py
+++ camera.py
@@ -71,9 +71,6 @@ class Camera:
         self.position = new_pos
     
     def do_floorcast_to_surface(self, distances, level, textures):
-
-        # Downscale
-        step = 1
         
         # Create a surface
         surface = pygame.Surface(WORKING_SIZE)
@@ -82,12 +79,59 @@ class Camera:
         # Compute angle mod
         angle_mod = np.math.atan2((WORKING_SIZE[0] / 2), DISTANCE_TO_PROJECTION_PLANE)
 
+        # Compute the left-most ray and right-most ray
+        left_ray = compute_direction(self.angle - angle_mod)
+        right_ray = compute_direction(self.angle + angle_mod)
+
+        # Generate rows
+        rows = np.arange(WORKING_SIZE[1])
+        look_offsets = (rows - WORKING_SIZE[1]/2)[:, np.newaxis]
+
+        # Compute h_distances
+        h_distances = np.empty_like(look_offsets)
+
+        middle = int(WORKING_SIZE[1]/2)
+        h_distances[:middle, 0] = (level.tile_size[2] - self.height) * DISTANCE_TO_PROJECTION_PLANE / (look_offsets[:middle,0] * np.math.cos(angle_mod))
+        h_distances[middle+1:, 0] = (self.height) * DISTANCE_TO_PROJECTION_PLANE / (look_offsets[middle+1:,0] * np.math.cos(angle_mod))
+
+        # Compute ray hits
+        ray_hit_left = (np.tile(left_ray, h_distances.shape) * h_distances)
+        ray_hit_right = (np.tile(right_ray, h_distances.shape) * h_distances)
+
+        # Do linear interpolation for ray hits
+        #ray_hits_x = np.linspace(ray_hit_left[:, 0], ray_hit_right[:, 0], WORKING_SIZE[0], axis=1)
+        #ray_hits_y = np.linspace(ray_hit_left[:, 1], ray_hit_right[:, 1], WORKING_SIZE[0], axis=1)
+
+        # Do linear interpolation for ray hits on floor
+        ray_hit = np.hstack([ray_hit_left, ray_hit_right])
+        ray_hits = np.linspace(ray_hit[:, 0:2], ray_hit[:, 2:3], WORKING_SIZE[0], axis=1)
+
+        # Compute which cells on the floor we hit and get the right textures
+        # ...
+
+        # Compute texture_xys
+        tex_size = np.array([TEXTURE_SIZE[0], TEXTURE_SIZE[1]])[np.newaxis, np.newaxis, :]
+        texture_xys = (np.floor(ray_hits) % tex_size).astype(int)
+
+        # Load texture
+        texture = surfarray.array2d(textures[15])
+
+        # Compute scanlines
+        scanlines = texture[texture_xys[:,:,0], texture_xys[:,:,1]]
+
+        # Set surface to scanlines
+        surface_array[:, :] = scanlines[:, :].T
+
+        # Delete surface array
+        del surface_array
+
+        return surface
+
+        print(scanlines.shape)
+
         # Iterate over each horisontal row
         for row in range(0, WORKING_SIZE[1]):
 
-            # Compute the left-most ray and right-most ray
-            left_ray = compute_direction(self.angle - angle_mod)
-            right_ray = compute_direction(self.angle + angle_mod)
 
             # Compute how many ray "extensions" for the ray to hit the ground
             look_offset = row - WORKING_SIZE[1]/2
@@ -132,127 +176,6 @@ class Camera:
 
         # Return surface
         return surface
-            
-        '''
-        # Iterate over each vertical column
-        for col in range(0, WORKING_SIZE[0], 1):
-
-            # Compute relative angle based on column
-            angle_mod =  np.math.atan2((col - WORKING_SIZE[0] / 2), DISTANCE_TO_PROJECTION_PLANE) # Slower but no curved edges
-            rayDirection = compute_direction(self.angle + angle_mod).reshape((1, -1))
-
-            # Extract the distance to the thingy from the thiny
-            distance = distances[col][0]
-
-            # Compute the height and height_offset
-            column_height, column_offset = self.column_height_from_distance(level, distance)
-
-            # Generate numpy array of certain size to contain row counts
-            nrows = WORKING_SIZE[1] - math.floor(column_height + column_offset)
-            rowarray = np.arange(math.floor(column_height + column_offset), WORKING_SIZE[1], 1)
-
-            # Now compute the look offsets
-            look_offsets = rowarray - WORKING_SIZE[1]/2
-
-            # Now compute horisontal distances
-            h_distances = (1/look_offsets) * ((self.height * DISTANCE_TO_PROJECTION_PLANE) / np.math.cos(angle_mod))
-            h_distances = h_distances.reshape((-1, 1))
-
-            # Now compute the xy_distances
-            xy_distances = h_distances @ rayDirection
-
-            # Compute level_xy
-            level_xy = (xy_distances + np.tile(self.position, (xy_distances.shape[0], 1)))
-            level_xy = np.transpose(level_xy) / np.array([level.tile_size[0], level.tile_size[1]])[:, np.newaxis]
-
-            # Compute cell_xy
-            cell_xy = np.floor(level_xy).T.astype(int)
-
-            # Compute texture_xy
-            texture_xy = (level_xy * np.array([level.tile_size[0], level.tile_size[1]])[:, np.newaxis]) % np.array([TEXTURE_SIZE[0], TEXTURE_SIZE[1]])[:, np.newaxis]
-            texture_xy = np.floor(texture_xy).T.astype(int)
-
-            # Compute the textures array
-            floor = np.array(level.floors) # REFACTOR THIS
-
-            floor_texture_ids = floor[cell_xy[:,[0]], cell_xy[:,[1]]] # NOT USED YET
-
-            texture = surfarray.array2d(textures[14])
-            floor_pixels = texture[texture_xy[:,[0]], texture_xy[:,[1]]]
-
-            # Draw using pixel references
-            surface_array[col, math.floor(column_height + column_offset):WORKING_SIZE[1]] = floor_pixels[:,0]
-
-        del surface_array
-
-        # Return surface
-        return surface        
-
-    # Now draw everything
-    '''
-    '''
-    for row in range(nrows):
-
-        # Extract cell_xy and texture_xy
-        cell = cell_xy[row, :]
-        texture = texture_xy[row, :]
-
-        # Now we feast, get texture
-        cellx = cell[0].astype(int)
-        celly = cell[1].astype(int)
-
-        texture_id = 0
-        if not(cellx < 0 or celly < 0 or celly >= len(level.floors) or cellx >= len(level.floors[celly])):
-            texture_id = level.floors[celly][cellx]
-        
-        # Now draw that to the pixel on the screen
-        pixel.blit(textures[texture_id], (-texture[0], -texture[1]))
-
-        # Draw pixel to surface
-        surface.blit(pygame.transform.scale(pixel, (step, step)), (col, row + math.floor(column_height + column_offset)))
-    '''
-
-    '''
-    # Now work for each floor pixel
-    for row in range(math.floor(column_height + column_offset), WORKING_SIZE[1], step):
-
-        # Compute the horisontal distance on the floor/horisontal distance to where intersection with floor occurs
-        look_offset = (row - WORKING_SIZE[1]/2)
-
-        if look_offset == 0:
-            continue
-
-        h_distance = (self.height * DISTANCE_TO_PROJECTION_PLANE) / (look_offset * np.math.cos(angle_mod))
-
-        # Now we part this into x and y distances using player angle
-        # xy_distance = self.direction * h_distance
-        xy_distance = rayDirection * h_distance
-
-
-        # Now compute the exact cell
-        levelX = (xy_distance[0] + self.position[0]) / level.tile_size[0]
-        levelY = (xy_distance[1] + self.position[1]) / level.tile_size[1]
-
-        cellX = math.floor(levelX)
-        cellY = math.floor(levelY)
-
-        texture_id = 0
-        if not(cellX < 0 or cellY < 0 or cellY >= len(level.floors) or cellX >= len(level.floors[cellY])):
-            texture_id = level.floors[cellY][cellX]
-
-        # Now compute the texture x and y
-        textureX = levelX * level.tile_size[0] / step % TEXTURE_SIZE[0]
-        textureY = levelY * level.tile_size[1] / step % TEXTURE_SIZE[1]
-
-
-        # Now draw that to the pixel on the screen
-        pixel.blit(textures[texture_id], (-textureX, -textureY))
-
-        # Draw pixel to surface
-        surface.blit(pygame.transform.scale(pixel, (step, step)), (col, row))
-
-    # We have our own direction vector, now compute the look angle
-    '''
         
         
     def do_raycast(self, level):
