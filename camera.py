Fixed a bug on sprite vertical placing and size scaling

camera.py
@@ -145,16 +145,24 @@ class Camera:
         # Compute horisontal offsets when drawing sprites on screen
         sprite_screen_xs = ((WORKING_SIZE[0]/2) * (1 + sprite_positions[:, [0]] / sprite_positions[:, [1]])).astype(int)
 
+        # Compute vertical offset when drawing sprite based on sprites desired height/z-pos, player height, player tilt and distance
+        sprite_heights, sprite_offsets = self.height_and_offset_from_distance(sprites_with_distances[:, [2]], sprite_positions[:, [1]] * level.tile_size[0])
+        sprite_heights = np.clip(sprite_heights, 0, 2*WORKING_SIZE[1])
+        sprite_offsets += self.tilt_offset
+
+        # sprites_with_distances[:, [2]]
+        #sprite_heights, sprite_offsets = self.height_and_offset_with_alignment(level.tile_size[2], sprite_positions[:, [1]] * level.tile_size[2], 0.)
+        #sprite_heights = np.clip(sprite_heights * (sprites_with_distances[:, [2]] / TEXTURE_SIZE[1]), 0, 2*WORKING_SIZE[1])
+        #sprite_offsets += self.tilt_offset
+
         # Compute sprite dimensions
-        sprite_heights = np.clip(np.abs(WORKING_SIZE[1] / sprite_positions[:, [1]]).astype(int), 0, 2*WORKING_SIZE[1])
+        #sprite_heights = np.clip(np.abs(WORKING_SIZE[1] / sprite_positions[:, [1]]).astype(int), 0, 2*WORKING_SIZE[1])
         sprite_sizes = np.hstack([sprite_heights, sprite_heights])
 
-        # Compute vertical offset when drawing sprite based on sprites desired height/z-pos, player height, player tilt and distance
-        _, sprite_offsets = self.height_and_offset_from_distance(sprites_with_distances[:, [2]], sprite_positions[:, [1]] * level.tile_size[0])
-        sprite_offsets += self.tilt_offset
         
         # Compute draw_start and draw_end
-        sprite_draw_start = np.hstack([sprite_screen_xs - (sprite_sizes[:, [0]]/2), sprite_offsets - (sprite_sizes[:, [1]]/2)])
+        #sprite_draw_start = np.hstack([sprite_screen_xs - (sprite_sizes[:, [0]]/2), sprite_offsets - (sprite_sizes[:, [1]]/2)])
+        sprite_draw_start = np.hstack([sprite_screen_xs - (sprite_sizes[:, [0]]/2), sprite_offsets])
         sprite_draw_end = sprite_draw_start + sprite_sizes
 
         # Build sprite data
@@ -359,6 +367,12 @@ class Camera:
     def height_and_offset_from_distance(self, true_height, distance):
         height = (true_height / distance) * DISTANCE_TO_PROJECTION_PLANE
         offset =  WORKING_SIZE[1]/2 - height + ((self.height / distance) * DISTANCE_TO_PROJECTION_PLANE)
+        return (height, offset)
+    
+    def height_and_offset_with_alignment(self, true_height, distance, v_offset):
+        height = (true_height / distance) * DISTANCE_TO_PROJECTION_PLANE
+        v_height = ((true_height + v_offset) / distance) * DISTANCE_TO_PROJECTION_PLANE
+        offset =  WORKING_SIZE[1]/2 - v_height + ((self.height / distance) * DISTANCE_TO_PROJECTION_PLANE)
         return (height, offset)
     
     '''

level.py
@@ -5,7 +5,10 @@ class Level:
     def __init__(self):
 
         self.tile_size = np.array([32, 32, 64])
-        self.ceiling_height = 128
+        self.ceiling_height = 96
+
+        self.player_start = (2.5, 13.5, np.pi + np.pi/2)
+        #self.player_start_dir = (np.pi/2)
 
         # Each wall is defined by a tag / wall type, a texture and height
         # type 0 == empty space / not wall
@@ -29,23 +32,23 @@ class Level:
         W11 = (1, 7, 64)    # brick_texture_03
 
         self.walls = np.array([
-            [W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],
-            [W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0],
+            # 00   01   02   03   04   05   06   07   08   09   10   11   12   13   14   15
+            [W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0],   # 00
+            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 01
+            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 02
+            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 03
+            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 04
+            [W_0, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 05
+            [W_6, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 06
+            [W_6, W_6, W_6, W_6, W_6, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 07
+            [W_6, AIR, AIR, AIR, W_7, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 08
+            [W_6, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 09
+            [W_6, AIR, AIR, AIR, W_7, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 10
+            [W_0, W_6, AIR, W_6, W_6, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 11
+            [W_0, W_6, AIR, W_6, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 12
+            [W_0, W_6, AIR, W_6, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 13
+            [W_0, W_6, W_6, W_6, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, AIR, W_0],   # 14
+            [W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0, W_0],   # 15
         ])
 
 
@@ -110,9 +113,9 @@ class Level:
         ])
 
         # Each sprite is defined by a sprite object (tag/type, texture, height/z-pos) and a position in the game world (x, y)
-        BARREL = [1, 0, 32]
-        PILLAR = [1, 1, 32]
-        LAMP = [0, 2, 64]
+        BARREL = [1, 0, 64]
+        PILLAR = [1, 1, 64]
+        LAMP = [0, 2, 96]
 
         sprite_positions = np.array([
             [6.5, 5.5],

main.py
@@ -11,11 +11,15 @@ from texture_helper import *
 
 from level import *
 
+# Font for writing text
+pygame.font.init()
+main_font = pygame.font.SysFont("Dejavu Sans", 16)
+
 # Level
 level = Level()
 
 # Game active objects
-player = camera.Camera(position=(8. * level.tile_size[0], 7. * level.tile_size[1]), angle=np.math.pi)
+player = camera.Camera(position=(level.player_start[0] * level.tile_size[0], level.player_start[1] * level.tile_size[1]), angle=level.player_start[2])
 
 # Initialize pygame window
 pygame.init()
@@ -30,6 +34,9 @@ sprite_textures = load_sprite_textures()
 
 # Make clock
 clock = pygame.time.Clock()
+
+# Mark debug info as not shown
+show_debug = False
 
 # Mark mouse as not grabbed
 mouse_grabbed = False
@@ -63,6 +70,10 @@ while (running):
                 pygame.event.set_grab(not(pygame.event.get_grab()))
                 pygame.mouse.set_pos(WINDOW_SIZE[0]/2, WINDOW_SIZE[1]/2)
                 mouse_grabbed = not(mouse_grabbed)
+            
+            # If X toggle debug info
+            if event.key == pygame.K_x:
+                show_debug = not(show_debug)
 
         if event.type == pygame.KEYUP:
 
@@ -186,7 +197,16 @@ while (running):
                 # Blit to screen
                 screen.blit(column, (col, draw_start[1]))
 
-                
+    
+    # Draw debug information to screen
+    if show_debug:
+        debug_text = "pos: {0}\ndir: {1}\nang: {2}".format(np.around(np.hstack([player.position, player.height]), 2), np.around(player.direction, 2), np.around(player.angle, 2))
+        debug_info = [main_font.render(line, False, (255, 255, 255)) for line in debug_text.split("\n")]
+        heightsum = 0
+        for info in debug_info:
+            screen.blit(info, (2, heightsum))
+            heightsum += info.get_height()
+
     # Paste screen frame
     frame = pygame.transform.scale(screen, WINDOW_SIZE)
     window.blit(frame, frame.get_rect())